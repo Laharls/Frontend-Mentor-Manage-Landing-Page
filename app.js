2231cc25ca20be879cf32f677beed15d1e57333d
Anthony <anthony089@outlook.fr>
2022-09-06 18:05:20 +0200

    feature: Add listener to check if email is valid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// function openMobileNavigation(){
-
     const menuBtn = document.querySelector("#hamburger_logo");
+    const inboxBtn = document.querySelector("#inbox_button");
+    let inboxInput = document.querySelector("#inbox_input");
     let menuOpen = false;
 
     menuBtn.addEventListener("click", () => {
@@ -15,4 +15,17 @@
             menuOpen = false;
         }
     })
-// }
+
+    inboxBtn.addEventListener("click", () => {
+        if(inboxInput.value.length === 0){
+            alert("Input empty");
+            return 
+        }
+
+        if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inboxInput.value))){
+            alert("Email not valid");
+            return
+        }
+
+        alert("Valid email, can proceed further");
+    })
